5fb7831410a142515a029e492618e50040ab34e1
captainPeanutbuttter495 <matthew.garcia.081@my.csun.edu>
2026-03-13 12:24:56 -0700

    added jest mocks

--- mobile/test/setup.js
+++ mobile/test/setup.js
@@ -1,1 +1,39 @@
 import "@testing-library/jest-native/extend-expect";
+
+jest.mock("expo-linear-gradient", () => {
+  const { View } = require("react-native");
+  return {
+    LinearGradient: ({ children, ...props }) => <View {...props}>{children}</View>,
+  };
+});
+
+jest.mock("react-native-svg", () => {
+  const { View } = require("react-native");
+  const mock = (name) => {
+    const C = ({ children, ...props }) => <View {...props}>{children}</View>;
+    C.displayName = name;
+    return C;
+  };
+  return {
+    __esModule: true,
+    default: mock("Svg"),
+    Svg: mock("Svg"),
+    Circle: mock("Circle"),
+    Defs: mock("Defs"),
+    Stop: mock("Stop"),
+    RadialGradient: mock("RadialGradient"),
+    G: mock("G"),
+  };
+});
+
+jest.mock("@expo/vector-icons", () => {
+  const { Text } = require("react-native");
+  const mockIcon = (name) => {
+    const Icon = (props) => <Text {...props}>{props.name}</Text>;
+    Icon.displayName = name;
+    return Icon;
+  };
+  return {
+    MaterialCommunityIcons: mockIcon("MaterialCommunityIcons"),
+  };
+});
