760cee62a504c038fecef2a151cf3cdf745823af
captainPeanutbuttter495 <matthew.garcia.081@my.csun.edu>
2026-03-13 14:16:05 -0700

    placeholder screens

---
+++ mobile/app/screens/FitbitScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function FitbitScreen() {
+  return (
+    <View className="flex-1 bg-background items-center justify-center px-6">
+      <Text className="text-base text-secondary">
+        This is the fitbit data page
+      </Text>
+    </View>
+  );
+}
---
+++ mobile/app/screens/NutritionScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function NutritionScreen() {
+  return (
+    <View className="flex-1 bg-background items-center justify-center px-6">
+      <Text className="text-base text-secondary">
+        This is the nutrition page
+      </Text>
+    </View>
+  );
+}
